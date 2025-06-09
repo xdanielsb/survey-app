c22340419cda4c60ca859b063c0af69b8f48b2e0
boss <daniel.further@gmail.com>
2025-06-09 17:00:27 +0200

    fix(backend): prevent keycloak to execute during test.

--- services/backend/src/test/java/com/survey/backend/controller/UserControllerTest.java
+++ services/backend/src/test/java/com/survey/backend/controller/UserControllerTest.java
@@ -9,11 +9,13 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.survey.backend.IntegrationTest;
 import com.survey.backend.entity.User;
 import com.survey.backend.repository.UserRepository;
+import com.survey.backend.service.KeycloakAdminService;
 import java.util.Map;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.ActiveProfiles;
@@ -27,6 +29,7 @@ public class UserControllerTest extends IntegrationTest {
   @Autowired private MockMvc mockMvc;
   @Autowired private ObjectMapper objectMapper;
   @Autowired private UserRepository userRepository;
+  @MockBean private KeycloakAdminService keycloakAdminService;
 
   @Test
   void createUser_savesUserAndReturnsOk() throws Exception {
