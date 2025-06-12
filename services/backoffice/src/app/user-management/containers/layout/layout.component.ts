723aecd426893c04738679a28750ccc63e4a8dc0
boss <daniel.further@gmail.com>
2025-06-12 22:23:29 +0200

    fix: sidenavs backoffice.

--- services/backoffice/src/app/user-management/containers/layout/layout.component.ts
+++ services/backoffice/src/app/user-management/containers/layout/layout.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core'
 import { RouterModule, RouterOutlet } from '@angular/router'
 import { NgIf } from '@angular/common'
-import { MatSidenavModule } from '@angular/material/sidenav'
+import { MatDrawer, MatSidenavModule } from '@angular/material/sidenav'
 import { MatToolbarModule } from '@angular/material/toolbar'
 import { MatButtonModule } from '@angular/material/button'
 import { MatIconModule } from '@angular/material/icon'
@@ -12,6 +12,7 @@ import { KeycloakService } from '../../services/keycloak.service'
   selector: 'app-layout',
   standalone: true,
   imports: [
+    MatDrawer,
     RouterModule,
     RouterOutlet,
     NgIf,
--- services/backoffice/src/app/user-management/containers/users/users.component.spec.ts
+++ services/backoffice/src/app/user-management/containers/users/users.component.spec.ts
@@ -1,6 +1,6 @@
 import { TestBed } from '@angular/core/testing'
 import { UsersComponent } from './users.component'
-import {HttpClientTestingModule} from '@angular/common/http/testing';
+import { HttpClientTestingModule } from '@angular/common/http/testing'
 
 describe('UsersComponent', () => {
   beforeEach(async () => {
